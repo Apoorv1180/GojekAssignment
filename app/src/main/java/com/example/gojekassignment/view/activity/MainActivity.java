d30f5dfd48a39a8d12834a77167caac4dcc78a3e
apoorv1180 <apoorv111221cse>
2019-11-15 18:01:17 +0530

    Activity Main XML Creation single repository item layout

--- app/src/main/java/com/example/gojekassignment/view/activity/MainActivity.java
+++ app/src/main/java/com/example/gojekassignment/view/activity/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.gojekassignment.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 import com.example.gojekassignment.R;
+import com.example.gojekassignment.viewmodel.RepositoryResponseViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    //viewModel
+    private RepositoryResponseViewModel repositoryResponseViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        repositoryResponseViewModel= ViewModelProviders.of(this).get(RepositoryResponseViewModel.class);
     }
 }
